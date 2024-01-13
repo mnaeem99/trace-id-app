5011401571c4eb9658c649ca25f618d88bcefa77
mnaeem99 <m.naeem9073@gmail.com>
2024-01-13 22:49:41 +0500

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.21"
+	kotlin("plugin.spring") version "1.9.21"
+}
+
+group = "com.trace"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.opentelemetry.api:opentelemetry-api:1.7.0")
+	implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-spring-boot-autoconfigure:1.7.0")
+	implementation("org.slf4j:slf4j-api:1.7.32")
+	implementation("org.slf4j:slf4j-simple:1.7.32")
+}
+
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
